40646be81e7e4009f58f590c56ce6285d1b2cb10
kichyr <kichyr@mail.ru>
2020-06-24 15:26:07 +0300

    New functional test were added.

--- test/funcitonal_test/functional_test.py
+++ test/funcitonal_test/functional_test.py
@@ -9,14 +9,51 @@ import shlex
 
 test_body = "gogog go \ngo"
 @pytest.mark.server(url='/test', response=[test_body])
-def test_web_resourse_deafult_goroutines():
+def test_web_resource_normal_response():
     cmd = "echo -e 'http://localhost:5000/test' | ./countgo"
-    process = subprocess.Popen(['bash', '-c', cmd], stdout=subprocess.PIPE)         
+    process = subprocess.Popen(['bash', '-c', cmd], stdout=subprocess.PIPE)
     out, _ = process.communicate()
     assert(out == b"Count for http://localhost:5000/test: 4\n")
 
-def test_web_resourse_one_goroutine():
+
+def test_web_resource_one_worker():
     cmd = "echo -e 'http://localhost:5000/test' | ./countgo -k=1"
-    process = subprocess.Popen(['bash', '-c', cmd], stdout=subprocess.PIPE)         
+    process = subprocess.Popen(['bash', '-c', cmd], stdout=subprocess.PIPE)
     out, _ = process.communicate()
     assert(out == b"Count for http://localhost:5000/test: 4\n")
+
+
+def test_file_resource_emty_file():
+    cmd = "echo -e './test/test_files/empty_test.txt' | ./countgo -k=1"
+    process = subprocess.Popen(['bash', '-c', cmd], stdout=subprocess.PIPE)
+    out, _ = process.communicate()
+    assert(out == b"Count for ./test/test_files/empty_test.txt: 0\n")
+
+
+def test_file_resource_normal_file():
+    cmd = "echo -e './test/test_files/simple_test.txt' | ./countgo -k=1"
+    process = subprocess.Popen(['bash', '-c', cmd], stdout=subprocess.PIPE)
+    out, _ = process.communicate()
+    assert(out == b"Count for ./test/test_files/simple_test.txt: 4\n")
+
+
+def test_multiple_resources():
+    multiple_resources_cmd = "echo -e \
+'./test/test_files/simple_test.txt\nhttp://localhost:5000/test\n\
+./test/test_files/empty_test.txt' | ./countgo -k=2"
+    process = subprocess.Popen(
+        ['bash', '-c', multiple_resources_cmd], stdout=subprocess.PIPE)
+    out, _ = process.communicate()
+    assert(
+        b'Count for ./test/test_files/simple_test.txt: 4' in out and
+        b'Count for ./test/test_files/empty_test.txt: 0' in out and
+        b'Count for http://localhost:5000/test: 4' in out
+    )
+
+
+def test_big_input_data():
+    cmd = "echo -e '" + "./test/test_files/simple_test.txt\n"*100 + "'\
+         | ./countgo"
+    process = subprocess.Popen(['bash', '-c', cmd], stdout=subprocess.PIPE)
+    out, _ = process.communicate()
+    assert(out.count(b'Count for ./test/test_files/simple_test.txt: 4') == 100)
